556eaf59beb7c84f1226faf28dc1888300c9beef
Sajjad Doosty <sajjad.doosty.83@gmail.com>
2025-04-15 23:31:32 +0330

    Add project files.

---
+++ DotnetPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿#region Min Number
+
+Console.Write("Enter count of number's: ");
+
+var count = Convert.ToInt32(Console.ReadLine());
+
+int minNumber = 0;
+
+for (int i = 0; i < count; i++)
+{
+	var number = Convert.ToInt32(Console.ReadLine());
+
+	if (i == 0)
+	{
+		minNumber = number;
+		continue;
+	}
+
+	if (number < minNumber)
+	{
+		minNumber = number;
+	}
+}
+
+Console.WriteLine(minNumber);
+
+#endregion
